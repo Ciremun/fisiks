2bebbc7c78141fc4f54e44c5a3caf31f149f2a8a
Ciremun <Ciremun@yandex.ru>
2021-11-08 12:22:14 +0300

    try fix

--- src/event.c
+++ src/event.c
@@ -81,11 +81,7 @@ void EXPORT("HandleButton") HandleButton(int x, int y, int button, int bDown)
 
 void EXPORT("HandleMotion") HandleMotion(int x, int y, int mask)
 {
-#ifndef __ANDROID__
-    if (!mask)
-        return;
-#endif
-    controls.lmb_down = !mask;
+    controls.lmb_down = mask;
     controls.mouse_x = x;
     controls.mouse_y = y;
 }
